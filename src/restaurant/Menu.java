1ff706fa0b86fb8c7f0966d4df05fc25debbfe2f
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-05 00:26:57 -0500

    added methods to Menu.java, MenuItems.java but will alter upon rewatching studio

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -3,6 +3,18 @@ package restaurant;
 import java.util.ArrayList;
 import java.util.HashMap;
 public class Menu {
+    private ArrayList<MenuItem> items;
+    private boolean lastUpdated;
 
+    public static void main(String[] args) {
 
+    }
+    public void addItem(MenuItem item) {
+        if (!items.contains(item)) {
+            items.add(item);
+        }
+    }
+    public void removeItem(MenuItem item) {
+        items.remove()
+    }
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -5,4 +5,15 @@ public class MenuItem {
     String description;
     String category;
     boolean isNew;
+
+    public static void main(String[] args) {
+
+    }
+    public String isItemNew() {
+        /*find date of restaurant grand opening, assign the number of days from then to present day
+          to variable, subtract date this item debut from that, if the difference is less than 22 days
+          it will be considered new. Return "New item!"*/
+        return "Item is new!";
+    }
+
 }
